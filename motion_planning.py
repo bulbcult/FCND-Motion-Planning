68146d4b4eac6b38e9193abb763b10c698a98eb3
bulbcult <62406938+bulbcult@users.noreply.github.com>
2020-03-28 21:56:07 +0800

    Update motion_planning.py

--- motion_planning.py
+++ motion_planning.py
@@ -120,12 +120,18 @@ class MotionPlanning(Drone):
         self.target_position[2] = TARGET_ALTITUDE
 
         # TODO: read lat0, lon0 from colliders into floating point values
-        
+        lat0 = 37.792480 
+        lon0 = -122.397450
         # TODO: set home position to (lon0, lat0, 0)
-
+        self.set_home_position(lon0, lat0, 0)
+        
+        
         # TODO: retrieve current global position
+        global_position = [self._latitude, self._longitude, self._altitude]
+        
  
         # TODO: convert to current local position using global_to_local()
+        local_position = global_to_local(global_position, self.global_home)
         
         print('global home {0}, position {1}, local position {2}'.format(self.global_home, self.global_position,
                                                                          self.local_position))
@@ -136,27 +142,45 @@ class MotionPlanning(Drone):
         grid, north_offset, east_offset = create_grid(data, TARGET_ALTITUDE, SAFETY_DISTANCE)
         print("North offset = {0}, east offset = {1}".format(north_offset, east_offset))
         # Define starting point on the grid (this is just grid center)
+        
         grid_start = (-north_offset, -east_offset)
         # TODO: convert start position to current position rather than map center
-        
+        grid_start = (local_position[0], local_position[1])
         # Set goal as some arbitrary position on the grid
         grid_goal = (-north_offset + 10, -east_offset + 10)
         # TODO: adapt to set goal as latitude / longitude position and convert
-
+        grid_goal = (local_position[0] + 10, local_position[0] + 10)
+        
         # Run A* to find a path from start to goal
         # TODO: add diagonal motions with a cost of sqrt(2) to your A* implementation
         # or move to a different search space such as a graph (not done here)
         print('Local Start and Goal: ', grid_start, grid_goal)
         path, _ = a_star(grid, heuristic, grid_start, grid_goal)
         # TODO: prune path to minimize number of waypoints
-        # TODO (if you're feeling ambitious): Try a different approach altogether!
-
+        # We're using collinearity here
+        path = prune_path(path)
         # Convert path to waypoints
         waypoints = [[p[0] + north_offset, p[1] + east_offset, TARGET_ALTITUDE, 0] for p in path]
         # Set self.waypoints
         self.waypoints = waypoints
         # TODO: send waypoints to sim (this is just for visualization of waypoints)
+        pp = np.array(pruned_path)
+        plt.scatter(pp[:, 1], pp[:, 0])
         self.send_waypoints()
+
+    def prune_path(path):
+        pruned_path = [p for p in path]
+        i = 0
+        # We're using collinearity here
+        while i < len(pruned_path) - 2:
+            p1 = point(pruned_path[i])
+            p2 = point(pruned_path[i+1])
+            p3 = point(pruned_path[i+2])
+            if collinearity_check(p1, p2, p3):
+                pruned_path.remove(pruned_path[i+1])
+            else:
+                i += 1
+        return pruned_path
 
     def start(self):
         self.start_log("Logs", "NavLog.txt")
